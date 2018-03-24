eebee26c8e6947a0f46db5813fc1aac1ab49b0d6
Marcos Castaneda <marcosjcastaneda@gmail.com>
2018-03-24 01:13:37 -0400

    Adds working image picker controller and hides navigation bar

--- FruityML.playground/Contents.swift
+++ FruityML.playground/Contents.swift
@@ -9,6 +9,7 @@ public class FruitViewController : UIViewController {
     @IBOutlet weak var sunImageView: UIImageView!
     @IBOutlet weak var eyesImageView: UIImageView!
     @IBOutlet weak var hillsImageView: UIImageView!
+    @IBOutlet weak var backgroundImageView: UIImageView!
     
     // Photo button
     @IBOutlet weak var photoButton: UIButton!
@@ -16,8 +17,14 @@ public class FruitViewController : UIViewController {
     // Label
     @IBOutlet weak var messageLabel: UILabel!
     
+    // Game is active
+    private var isActive = true
+    
     // AVPlayer
-    private var player: AVAudioPlayer!
+//    private var player: AVAudioPlayer!
+    
+    // Keep track of cancellations for an easter egg
+    var numberOfCancellations = 0
     
     // When an intro message is fully played out
     var hasWelcomedUser = false
@@ -27,9 +34,19 @@ public class FruitViewController : UIViewController {
         sunImageView.layer.opacity = 0.0
         eyesImageView.layer.opacity = 0.0
         messageLabel.alpha = 0.0
-//        photoButton.alpha = 0.0
+        photoButton.alpha = 0.0
         hillsImageView.alpha = 0.0
         eyesImageView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        
+        hideNavigationBar()
+    }
+    
+    // Make nav bar clear (required for image picker)
+    public func hideNavigationBar() {
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.view.backgroundColor = .clear
     }
     
     // View did appear
@@ -59,49 +76,27 @@ public class FruitViewController : UIViewController {
         
         sendMessage(message: "☉ Hello human! I'm the Sun, creator of all tasty fruits on your planet Earth", seconds: 2.0)
         sendMessage(message: "☉ I can tell you about any fruit that you send to me", seconds: 6.0)
-        sendMessage(message: "☉ Make sure to find a nearby fruit on Earth to begin", seconds: 10.0)
-        sendMessage(message: "☉ Then use your metal goggles (camera) to send me a fruit to explore", seconds: 15.0)
+        sendMessage(message: "☉ Make sure to find a nearby fruit to begin", seconds: 10.0)
+        sendMessage(message: "☉ Then use your metal goggles (camera) to send me a fruit to explore", seconds: 14.0)
         
         // Welcome completed
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.5 + 7.0 + 7.0) {
-            print("Welcome")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.5 + 7.0 + 6.5) {
             self.hasWelcomedUser = true
             self.setupInteractivity()
         }
     }
     
     @IBAction func takePicture() {
-        print("Picture did touch")
-        
-//        let alert = UIAlertController(title: "Title", message: "Message", preferredStyle: .alert)
-//
-//        alert.addAction(UIAlertAction(title: "Yes", style: .destructive, handler: { _ in
-//        }))
-//
-//        alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { _ in
-//        }))
-//
-//        (self.parent as! UINavigationController).present(alert, animated: true, completion: nil)
-        
-//        let picker = UIImagePickerController()
-//        picker.allowsEditing = false
-//        picker.sourceType = .photoLibrary
-//        picker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
-//        picker.modalPresentationStyle = .popover
-//        present(picker, animated: true, completion: nil)
-        
-//        picker.popoverPresentationController?.barButtonItem = sender
+        if (!self.hasWelcomedUser) {
+            return
+        }
 
-//        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
-//            presentPhotoPicker(sourceType: .photoLibrary)
-//            return
-//        }
-//
-        
         // Important to cast in order to send alerts/segues
         let parentView = self.parent as! UINavigationController
         
         let photoSourcePicker = UIAlertController()
+
+        // Assume camera is available
         let takePhoto = UIAlertAction(title: "Take Photo", style: .default) { _ in
             self.presentPhotoPicker(sourceType: .camera)
         }
@@ -113,6 +108,7 @@ public class FruitViewController : UIViewController {
         photoSourcePicker.addAction(takePhoto)
         photoSourcePicker.addAction(choosePhoto)
         
+        // Hacky fix to prevent crashing on Playground
         if let popoverController = photoSourcePicker.popoverPresentationController {
             popoverController.sourceView = view
             popoverController.sourceRect = CGRect(x: 0, y: view.bounds.size.height, width: view.bounds.size.width, height: 20.0)
@@ -122,6 +118,7 @@ public class FruitViewController : UIViewController {
         parentView.present(photoSourcePicker, animated: true)
     }
     
+    // Present image picker
     func presentPhotoPicker(sourceType: UIImagePickerControllerSourceType) {
         let picker = UIImagePickerController()
         picker.delegate = self
@@ -147,7 +144,7 @@ public class FruitViewController : UIViewController {
                 self.messageLabel.text = message
 
                 // Fade in
-                UIView.animate(withDuration: 0.5, delay: 0.5, options: UIViewAnimationOptions.curveEaseInOut, animations: {
+                UIView.animate(withDuration: 0.5, delay: 0.25, options: UIViewAnimationOptions.curveEaseInOut, animations: {
                     self.messageLabel.alpha = 1.0
                 }, completion: { _ in
                     self.playSound()
@@ -156,8 +153,8 @@ public class FruitViewController : UIViewController {
         }
     }
     
+    // TODO: Add sound
     public func playSound() {
-  
     }
     
     // Rotate sun
@@ -165,7 +162,7 @@ public class FruitViewController : UIViewController {
         UIView.animate(withDuration: 6.0, delay: 0.0, options: UIViewAnimationOptions.curveLinear, animations: {
             self.sunImageView.transform = self.sunImageView.transform.rotated(by: CGFloat(Double.pi))
         }) { _ in
-            self.rotateSun()
+            self.isActive ? self.rotateSun() : nil
         }
     }
     
@@ -176,7 +173,7 @@ public class FruitViewController : UIViewController {
         }, completion: { _ in UIView.animate(withDuration: 0.3, delay: 0.0, options: UIViewAnimationOptions.curveEaseOut, animations: {
             self.eyesImageView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
         }, completion: { _ in
-            self.blinkEyes()
+            self.isActive ? self.blinkEyes() : nil
         })
         })
     }
@@ -186,9 +183,49 @@ extension FruitViewController: UIImagePickerControllerDelegate, UINavigationCont
     // MARK: - Handling Image Picker Selection
     
     public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.numberOfCancellations += 1
+        
+        let phrases = ["☉ No fruit, human? This is the last time I waste my time on Earth",
+                       "☉ I always forget how unreliable you humans are",
+                       "☉ Cancel on me again and watch what happens to your planet",
+                       "☉ Forget it, human, I hear there's life on Mars I can test on",
+                       "☉ Look, human, I need your dedication to this experiment",
+                       "☉ Send me a photo or I'm leaving to another planet",
+                       "☉ Your species was never as smart as the colony on Jupit... Nevermind"]
+        let i = Int(arc4random_uniform(UInt32(phrases.count)))
+
         picker.dismiss(animated: true) {
-            self.sendMessage(message: "☉ No fruit, human? This is the last time I waste my time on Earth", seconds: 0.0)
+            if (self.numberOfCancellations > 2) {
+                self.sendMessage(message: "You tested my patience human, goodbye!", seconds: 0.0)
+                self.quitGame()
+            } else {
+                self.sendMessage(message: phrases[i], seconds: 0.0)
+            }
         }
+    }
+    
+    // Easter egg to end the game
+    public func quitGame() {
+        self.view.backgroundColor = UIColor.black
+        
+        isActive = false
+        
+        UIView.animate(withDuration: 0.5) {
+            self.photoButton.alpha = 0.0
+            self.eyesImageView.transform = CGAffineTransform(scaleX: 0, y: 0)
+            self.sunImageView.transform = CGAffineTransform(scaleX: 0, y: 0)
+        }
+        
+        UIView.animate(withDuration: 1.5, animations: {
+            self.hillsImageView.alpha = 0.2
+        })
+        
+        UIView.animate(withDuration: 3.0) {
+            self.backgroundImageView.alpha = 0.0
+            self.messageLabel.backgroundColor = UIColor(white: 1.0, alpha: 0.3)
+            self.sendMessage(message: "Brrr...", seconds: 0.0)
+        }
+        
     }
     
     public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {

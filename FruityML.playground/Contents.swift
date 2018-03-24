9a2afc15e05e2c30a0b80a229657ab913b870ef3
Marcos Castaneda <marcosjcastaneda@gmail.com>
2018-03-23 20:22:44 -0400

    Adds slightly buggy eye blinks for UI

--- FruityML.playground/Contents.swift
+++ FruityML.playground/Contents.swift
@@ -1,8 +1,10 @@
-//: A UIKit based Playground for presenting user interface
+//#-hidden-code
   
 import UIKit
 import PlaygroundSupport
 import AVFoundation
+
+var didInit = false
 
 @objc(FruitViewController)
 public class FruitViewController : UIViewController {
@@ -25,11 +27,18 @@ public class FruitViewController : UIViewController {
     
     // View will appear
     public override func viewDidLoad() {
+        if (didInit) {
+            fatalError()
+        } else {
+            didInit = true
+        }
+        
         sunImageView.layer.opacity = 0.0
         eyesImageView.layer.opacity = 0.0
         messageLabel.alpha = 0.0
         photoButton.alpha = 0.0
         hillsImageView.alpha = 0.0
+        eyesImageView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
     }
     
     // View did appear
@@ -55,12 +64,13 @@ public class FruitViewController : UIViewController {
     
     // Start messages
     public func startWelcome() {
-        sayMessage(message: "☉ Hello human subject! I'm the Sun, creator of all tasty fruits on your planet Earth", seconds: 2.0)
-        sayMessage(message: "☉ I can tell you all about any fruit that you send to me", seconds: 7.0)
-        sayMessage(message: "☉ Make sure to find a nearby fruit on Earth to begin", seconds: 14.0)
+        blinkEyes()
+        
+        sayMessage(message: "☉ Hello human! I'm the Sun, creator of all tasty fruits on your planet Earth", seconds: 2.0)
+        sayMessage(message: "☉ I can tell you about any fruit that you send to me", seconds: 7.0)
+        sayMessage(message: "☉ Make sure to find a nearby fruit on Earth to begin", seconds: 13.0)
         sayMessage(message: "☉ Then use your metal goggles (camera) to send me a fruit to explore", seconds: 20.0)
         
-        print("...")
         // Welcome completed
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.5 + 7.0 + 14.0) {
             print("Welcome")
@@ -86,39 +96,69 @@ public class FruitViewController : UIViewController {
                 // Replace label
                 self.messageLabel.text = message
                 
-
+                
                 
                 // Fade in
                 UIView.animate(withDuration: 0.5, delay: 0.5, options: UIViewAnimationOptions.curveEaseInOut, animations: {
                     
                     // create a sound ID, in this case its the tweet sound.
-                   
-
+                    
+                    
                     self.messageLabel.alpha = 1.0
                 }, completion: { finished in
-                     self.playSound()
-                    })
+                    self.playSound()
+                })
             })
         }
     }
     
     public func playSound() {
-        let systemSoundID: SystemSoundID = 1016
-        AudioServicesPlaySystemSound(systemSoundID)
+        guard let url = Bundle.main.url(forResource: "alert", withExtension: "mp3") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            
+            player = try AVAudioPlayer(contentsOf: url)
+            guard let player = player else { return }
+            
+            player.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
     
     // Rotate sun
     public func rotateSun() {
         UIView.animate(withDuration: 6.0, delay: 0.0, options: UIViewAnimationOptions.curveLinear, animations: {
             self.sunImageView.transform = self.sunImageView.transform.rotated(by: CGFloat(Double.pi))
-        }) { completed in
+        }) { done in
             self.rotateSun()
         }
     }
+    
+    // Blink the sun's eyes (careful they're hot)
+    public func blinkEyes() {
+        UIView.animate(withDuration: 0.3, delay: 5.0, options: UIViewAnimationOptions.curveEaseInOut, animations: {
+            self.eyesImageView.transform = CGAffineTransform(scaleX: 1.0, y: 0.1)
+        }, completion: { done in UIView.animate(withDuration: 0.3, delay: 0.0, options: UIViewAnimationOptions.curveEaseOut, animations: {
+            self.eyesImageView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        }, completion: { done in
+            self.blinkEyes()
+        })
+        })
+    }
 }
+//#-end-hidden-code
 
 let storyboard = UIStoryboard(name: "Main", bundle: nil)
 let view = storyboard.instantiateViewController(withIdentifier: "MainView") as! FruitViewController
 
 // Present the view controller in the Live View window
 PlaygroundPage.current.liveView = view
+
+/*:
+ Testing one, two three.
+ */
